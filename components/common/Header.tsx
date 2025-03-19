697fa94c2be8d4c052926c3e24e231ee924b9139
Akash Kumar <akash19g245@gmail.com>
2025-03-19 22:38:03 +0530

    Navbar completed

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,34 +1,40 @@
-import Link from "next/link"
+
 import { FileText } from 'lucide-react';
+import { Button } from "@/components/ui/button";
+import NavLink from "@/components/common/nav-link";
+
+
 
 export default function Header(){
     const isLogged = false;
     return(
-       <nav className="container flex items-center justify-between py-4 lg:px-8 px-2 mx-auto">
-        <div className="flex lg:flex-1 group">
-            
-            <Link className="flex items-center gap-1 lg:gap-2 shrink-0" href="/"><FileText className="w-5 h-5 lg:w-8 lg:h-8 text-gray-900 group-hover:rotate-12 transfrom transition ease-in-out duration-300" />
-            <span className="font-extrabold lg:text-xl text-gray-900">Summarize</span>
-            </Link>
-        </div>
-
-        <div className="flex lg:justify-center gap-4 lg:gap-12">
-            <Link href="/#pricing">Pricing</Link>
-            <Link href="/dashboard">Your Summaries</Link>
-        </div>
-
-        <div className="flex lg:justify-end lg:flex-1">
-            {isLogged ? (
-                <div className="flex gap-2 items-center">
-                <Link href="/upload">Upload a PDF</Link>
-                </div>
-            ) : (
-                <div>
-            <Link href="/sign-in">Sign In</Link>
+        <nav className="w-full flex items-center justify-between py-4 lg:px-8 px-2 mx-auto">
+            <div className="flex lg:flex-1 group">
+                
+                <NavLink className="flex items-center gap-1 lg:gap-2 shrink-0" href="/"><FileText className="w-5 h-5 lg:w-8 lg:h-8 text-gray-900 group-hover:rotate-12 transfrom transition ease-in-out duration-300" />
+                <span className="font-extrabold lg:text-xl text-gray-900">Summarize</span>
+                </NavLink>
             </div>
-            ) }
-            
-        </div>
-       </nav> 
+
+            <div className="flex lg:justify-center gap-4 lg:gap-12">
+                <NavLink href="/#pricing">Pricing</NavLink>
+                {isLogged && <NavLink href="/dashboard">Your Summaries</NavLink>}
+            </div>
+
+            <div className="flex lg:justify-end lg:flex-1">
+                {isLogged ? (
+                    <div className="flex gap-2 items-center">
+                    <NavLink href="/upload">Upload a PDF</NavLink>
+                    <div>Pro</div>
+                    <Button>User</Button>
+                    </div>
+                ) : (
+                    <div>
+                <NavLink href="/sign-in">Sign In</NavLink>
+                </div>
+                ) }
+                
+            </div>
+        </nav> 
     )
 }
