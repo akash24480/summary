859fcf693153ad6075235195d0fa114f86f539e5
Akash Kumar <akash19g245@gmail.com>
2025-03-19 23:50:17 +0530

    Working on the Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import HeroSection from "@/components/home/HeroSection";
 import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
     <div>
-      <Button variant="outline">Click me</Button>
+      <HeroSection />
+      {/* <DemoSection />
+      <HowItWorks />
+      <PricingSection />
+      <CtaSection /> */}
     </div>
   );
 }
