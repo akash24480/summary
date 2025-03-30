352260b212ece31461a312f204ac360316c2e23f
Akash Kumar <akash19g245@gmail.com>
2025-03-30 22:03:15 +0530

    working on uploader page

---
+++ app/(logged-in)/upload/page.tsx
@@ -0,0 +1,18 @@
+import BgGradient from "@/components/common/BgGradient";
+import UploadForm from "@/components/upload/UploadForm";
+
+import UploadHeader from "@/components/upload/UploadHeader";
+
+export default function Page(){
+    return (
+       <section className="min-h-screen">
+        <BgGradient />
+        <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
+        <div className="flex flex-col items-center justify-center gap-6">
+        <UploadHeader />
+        <UploadForm />
+        </div>
+        </div>
+       </section>
+    )
+}
--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -2,39 +2,44 @@
 import { FileText } from 'lucide-react';
 import { Button } from "@/components/ui/button";
 import NavLink from "@/components/common/nav-link";
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 
 
 
-export default function Header(){
-    const isLogged = false;
-    return(
+export default function Header() {
+    return (
         <nav className="w-full flex items-center justify-between py-4 lg:px-8 px-2 mx-auto">
             <div className="flex lg:flex-1 group">
-                
+
                 <NavLink className="flex items-center gap-1 lg:gap-2 shrink-0" href="/"><FileText className="w-5 h-5 lg:w-8 lg:h-8 text-gray-900 group-hover:rotate-12 transfrom transition ease-in-out duration-300" />
-                <span className="font-extrabold lg:text-xl text-gray-900">Summarize</span>
+                    <span className="font-extrabold lg:text-xl text-gray-900">Summarize</span>
                 </NavLink>
             </div>
 
             <div className="flex lg:justify-center gap-4 lg:gap-12">
                 <NavLink href="/#pricing">Pricing</NavLink>
-                {isLogged && <NavLink href="/dashboard">Your Summaries</NavLink>}
+                <SignedIn>
+                    <NavLink href="/dashboard">Your Summaries</NavLink>
+                </SignedIn>
             </div>
 
             <div className="flex lg:justify-end lg:flex-1">
-                {isLogged ? (
+                <SignedIn>
                     <div className="flex gap-2 items-center">
-                    <NavLink href="/upload">Upload a PDF</NavLink>
-                    <div>Pro</div>
-                    <Button>User</Button>
+                        <NavLink href="/upload">Upload a PDF</NavLink>
+                        <div>Pro</div>
+                        <SignedIn>
+                            <UserButton />
+                        </SignedIn>
                     </div>
-                ) : (
-                    <div>
-                <NavLink href="/sign-in">Sign In</NavLink>
-                </div>
-                ) }
-                
+                </SignedIn>
+
+                <SignedOut>
+                    <NavLink href="/sign-in">Sign In</NavLink>
+                </SignedOut>
+
+
             </div>
-        </nav> 
+        </nav>
     )
 }
